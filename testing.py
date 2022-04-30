59586a8e3b28a1b03b70cd4d788e7ac03ad8b2fd
tayyebsoubra <59086051+tayyebsoubra@users.noreply.github.com>
2022-04-30 16:35:29 +0300

    Added file open and the results.txt file

--- testing.py
+++ testing.py
@@ -5,15 +5,53 @@ from matplotlib import pyplot as plt
 import regex
 import numpy as np
 directory = 'C:/Users/tayeb/Desktop/New folder/training'
+counter = 0
+reader = easyocr.Reader(['en'])
 for filename in os.listdir(directory):
     if filename.endswith(".jpg"):
         IMAGE_PATH = os.path.join(directory,filename)
-        reader = easyocr.Reader(['en'])
-        result = reader.readtext(IMAGE_PATH,decoder='greedy',batch_size=50)
+        result = reader.readtext(IMAGE_PATH,decoder='wordbeamsearch',batch_size=50)
+        result1 = []
+        result2 = []
+        result3 = []
+        final_result = ''
+        for text in result:
+            pattern = regex.find_date_true(text[1])
+            if (pattern is not None):
+                result1.append(pattern)
+                print(pattern)
+        for text in result:
+            pattern = regex.find_date_test_CMP1(text[1])
+            if (pattern is not None):
+                result2.append(pattern)
+                print(pattern)
+        for text in result:
+            pattern = regex.find_date_Improved1(text[1])
+            if (pattern is not None):
+                result3.append(pattern)
+                print(pattern)
+        print(result1)
+        print(result2)
+        print(result3)
+        if len(result1) == 0:
+            b_set = set(result2)
+            c_set = set(result3)
+            final_result = c_set.intersection(b_set)
+            print(c_set.intersection(b_set))
+        if len(result1) > 1:
+            a_set = set(result1)
+            b_set = set(result2)
+            c_set = set(result3)
+            final_result = c_set.intersection(a_set.intersection(b_set))
+            print(c_set.intersection(a_set.intersection(b_set)))
+        for text in result:
+            pattern = regex.find_date_improved(text[1])
+            if (pattern is not None):
+                print(pattern)
+        img = cv2.imread(IMAGE_PATH)
         top_left = tuple(result[0][0][0])
         bottom_right = tuple(result[0][0][2])
         text = result[0][1]
-        img = cv2.imread(IMAGE_PATH)
         for detection in result:
             top_left = tuple([int(val) for val in detection[0][0]])
             bottom_right = tuple([int(val) for val in detection[0][2]])
@@ -27,8 +65,11 @@ for filename in os.listdir(directory):
             if (pattern is not None):
                 print(text[1])
                 print(pattern)
-        plt.figure(figsize=(10, 10))
-        plt.imshow(img)
-        plt.show()
+        counter = counter + 1
+        file_str = str(counter) + ': ' + str(final_result)
+        f = open('results.txt', 'a')
+        f.write(file_str)
+        f.write('\n')
+        f.close()
 
 
