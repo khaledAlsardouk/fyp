bf7a72e1dc5acf61900a4566e731e680d5735a23
Mohammad Kreidieh <master_nitrodox7@outlook.com>
2022-04-30 23:43:09 +0300

    updated flask_capture
    fixed async issue
    but
    the flash() function not working anymore :') (Cant show output on screen)
    but it does everything else fine

--- flask/flask_capture.py
+++ flask/flask_capture.py
@@ -4,6 +4,7 @@ import datetime, time
 import os
 import text_detection_1 as td
 global capture, switch, frame
+import sys
 
 
 capture=0       #to capture image
@@ -11,21 +12,36 @@ switch=0        #to turn the camera on and off
 app = Flask(__name__)
 app.secret_key = "secret key"
 url = 'http://192.168.2.237:8080/video'
-camera = cv2.VideoCapture(url)    #cahnge 0 to url for IP web
+camera = cv2.VideoCapture(0)    #cahnge 0 to url for IP web
 
 def generate_frames():      #camera
-    global frame
+    global frame, capture, switch
     while switch:
 
         ## read the camera frame
         success, frame = camera.read()
-        if not success:
-            break
+        if success:
+            if (capture):
+                capture=0
+                switch = 0
+                now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
+                image_path = './shots/' + now + '.jpg'
+                cv2.imwrite(image_path, frame)
+                #flash(str(image_path))           #shows the path
+                #print(image_path, file=sys.stderr)
+                flash(td.OCR_TD(image_path))
+
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-        yield (b'--frame\r\n'           
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            break
+
+
 
 
 
@@ -37,22 +53,18 @@ def video():
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    global switch, capture
-    success, frame = camera.read()
+    global switch
+    #success, frame = camera.read()
     if request.method == 'POST':
         if request.form.get('stop') == 'Stop/Start':
             if(switch==0):
                 switch = 1
-            else: switch = 0
-        elif request.form.get('click') == 'Capture':        #caotuer current frame
-            capture = 0
-            switch = 0
-            now = (datetime.datetime.now()).strftime("%f")  #generate name based on time
-            #image_path = os.path.sep.join(['shots', "shot_{}.jpg".format(str(now).replace(":", ''))])
-            image_path = './shots/' + now + '.jpg'
-            cv2.imwrite(image_path, frame)
-            #flash(image_path)           #shows the path
-            flash(td.OCR_TD(image_path))
+            else:
+                switch=0
+        elif request.form.get('click') == 'Capture':        #capture current frame
+            global capture
+            #flash('here1')
+            capture = 1
         else:
             pass  # unknown
     elif request.method == 'GET':
