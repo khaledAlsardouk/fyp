2d10f22ce8dfdabf1b734bbec62f9564182268b9
khaled <khaledsardouk@gmail.com>
2022-05-01 14:15:22 +0300

    cleaning unused files and code

--- flask/text_detection_1.py
+++ flask/text_detection_1.py
@@ -2,25 +2,12 @@ import easyocr
 import os
 import cv2
 from matplotlib import pyplot as plt
-import regex
 import numpy as np
 
 
 def OCR_TD(IMAGE_PATH):
         reader = easyocr.Reader(['en'], gpu=False)
         result = reader.readtext(IMAGE_PATH,decoder='greedy',batch_size=50)
-        top_left = tuple(result[0][0][0])
-        bottom_right = tuple(result[0][0][2])
-        text = result[0][1]
-        img = cv2.imread(IMAGE_PATH)
-        for detection in result:
-            top_left = tuple([int(val) for val in detection[0][0]])
-            bottom_right = tuple([int(val) for val in detection[0][2]])
-            text = detection[1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 5)
-            img = cv2.putText(img, text, top_left, font, .5, (255, 255, 255), 2, cv2.LINE_AA)
-
         return result
 
 
--- text_detection/testing.py
+++ text_detection/testing.py
@@ -19,20 +19,16 @@ for filename in os.listdir(directory):
             pattern = regex.find_date_true(text[1])
             if (pattern is not None):
                 result1.append(pattern)
-                print(pattern)
         for text in result:
             pattern = regex.find_date_test_CMP1(text[1])
             if (pattern is not None):
                 result2.append(pattern)
-                print(pattern)
+
         for text in result:
             pattern = regex.find_date_Improved1(text[1])
             if (pattern is not None):
                 result3.append(pattern)
-                print(pattern)
-        print(result1)
-        print(result2)
-        print(result3)
+
         if len(result1) == 0:
             b_set = set(result2)
             c_set = set(result3)
@@ -44,27 +40,6 @@ for filename in os.listdir(directory):
             c_set = set(result3)
             final_result = c_set.intersection(a_set.intersection(b_set))
             print(c_set.intersection(a_set.intersection(b_set)))
-        for text in result:
-            pattern = regex.find_date_improved(text[1])
-            if (pattern is not None):
-                print(pattern)
-        img = cv2.imread(IMAGE_PATH)
-        top_left = tuple(result[0][0][0])
-        bottom_right = tuple(result[0][0][2])
-        text = result[0][1]
-        for detection in result:
-            top_left = tuple([int(val) for val in detection[0][0]])
-            bottom_right = tuple([int(val) for val in detection[0][2]])
-            text = detection[1]
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 5)
-            img = cv2.putText(img, text, top_left, font, .5, (255, 255, 255), 2, cv2.LINE_AA)
-        print(result)
-        for text in result:
-            pattern = regex.find_parser(text[1])
-            if (pattern is not None):
-                print(text[1])
-                print(pattern)
         counter = counter + 1
         file_str = str(counter) + ': ' + str(final_result)
         f = open('results.txt', 'a')
