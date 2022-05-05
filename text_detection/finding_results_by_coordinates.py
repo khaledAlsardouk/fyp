5a43eadb5243b4c4fca2cd7d7b66c316732b48d4
tayyebsoubra <59086051+tayyebsoubra@users.noreply.github.com>
2022-05-05 16:05:13 +0300

    needs modifications to run

--- text_detection/finding_results_by_coordinates.py
+++ text_detection/finding_results_by_coordinates.py
@@ -13,9 +13,6 @@ def OCR_TD(IMAGE_PATH):
     return result
 
 
-result = OCR_TD(r'C:\Users\tayeb\Desktop\New folder\training\1.jpg')
-print(result)
-
 
 def is_date(string, fuzzy=False):
     try:
@@ -27,13 +24,32 @@ def is_date(string, fuzzy=False):
 
 
 def coordinates(result):
+    average = 1000000
     for x in result:
         file = open('expiry_date.txt', 'r')
         if result[x][1] in file.read():
             exp_string = result[x][1]
             exp_string = exp_string.split(":")[0]
             if is_date(exp_string):
+                exp_string = parse(exp_string)
                 return exp_string
             else:
-                Xb = result[0][1][0]
-                Yb = result[0][1][1]
+                Xb = result[x][0][1][0]
+                Yb = result[x][0][1][1]
+                for x in result:
+                    Xtemp = Xb - result[x][0][1][0]
+                    Ytemp = Yb - result[x][0][1][1]
+                    average_temp = (Xtemp + Ytemp)/2
+                    if average_temp < average:
+                        average = average_temp
+                        Xa = Xtemp
+                        Ya = Ytemp
+                for x in result:
+                    if ((result[x][0][1][0] == Xa) and (result[x][0][1][1] == Ya)):
+                        exp_string = parse(result[x][1])
+                        return exp_string
+
+
+
+result = OCR_TD(r'C:\Users\tayeb\Desktop\New folder\training\1.jpg')
+print(coordinates(result))
