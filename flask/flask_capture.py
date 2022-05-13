68d3e601ae945e735da9d6fab7722449b749e5d6
Mohammad Kreidieh <master_nitrodox7@outlook.com>
2022-05-13 23:33:48 +0300

    instructions and css
    added instructions for user (just a <p> not a popup)
    fixed css of the capture.html

--- flask/flask_capture.py
+++ flask/flask_capture.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, Response,request, flash
 import cv2
 import datetime, time
 import os
-import text_detection_1 as td
-global capture, switch, frame
+#import text_detection_1 as td
+global capture, switch, frame, BarOrExp
 import sys
 
 
@@ -15,6 +15,36 @@ app.secret_key = "secret key"
 url = 'http://192.168.2.237:8080/video'
 camera = cv2.VideoCapture(0)    #cahnge 0 to url for IP web
 
+def popups1():
+    global BarOrExp, barcode
+    if(barcode==1):
+        BarOrExp = "Please Scan The Barcode"
+    else:
+        BarOrExp = "Please Scan Expiry Date"
+    return BarOrExp
+
+
+def capture_exp_images():
+    global frame, capture, switch, barcode
+    capture = 0
+    now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
+    image_path = './shots/exp' + now + '.jpg'  # name for exp image
+    switch = 0  # turn off switch
+    camera.release()  # turn off camera
+    cv2.imwrite(image_path, frame)  # save barcode image
+    barcode = 1
+
+def capture_bar_images():
+    global frame, capture, switch, barcode
+    capture = 0
+    now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
+    image_path = './shots/bar' + now + '.jpg'  # name for barcode image
+    switch = 0  # turn off switch
+    camera.release()  # turn off camera
+    cv2.imwrite(image_path, frame)  # save barcode image
+    barcode = 0  # reset barcode turn
+
+
 def generate_frames():      #camera
     global frame, capture, switch,barcode
     while switch:
@@ -22,25 +52,27 @@ def generate_frames():      #camera
         ## read the camera frame
         success, frame = camera.read()
         if success:
-            if (capture and not barcode):
-                capture = 0
-                now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
-                image_path = './shots/exp' + now + '.jpg'       #name for exp image
-                switch = 0                                      #turn off switch
-                camera.release()                                #turn off camera
-                cv2.imwrite(image_path, frame)                  #save barcode image
-                barcode = 1                                     #set barcode turn
+            if (capture == 1 and barcode==0):
+                capture_exp_images()
+                # capture = 0
+                # now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
+                # image_path = './shots/exp' + now + '.jpg'       #name for exp image
+                # switch = 0                                      #turn off switch
+                # camera.release()                                #turn off camera
+                # cv2.imwrite(image_path, frame)                  #save barcode image
+                # barcode = 1                                     #set barcode turn
                 #flash(str(image_path))           #shows the path
                 #print(image_path, file=sys.stderr)
                 #flask.jsonify("help") #REUTRN EXP HERE HELPP HEREEEEEEEEEEEEEEEEEEEP
-            if (capture and barcode):
-                capture = 0
-                now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
-                image_path = './shots/bar' + now + '.jpg'       #name for barcode image
-                switch = 0                                      #turn off switch
-                camera.release()                                #turn off camera
-                cv2.imwrite(image_path, frame)                  #save barcode image
-                barcode = 0                                     #reset barcode turn
+            if (capture == 1 and barcode == 1):
+                capture_bar_images()
+                # capture = 0
+                # now = (datetime.datetime.now()).strftime("%f")  # generate name based on time
+                # image_path = './shots/bar' + now + '.jpg'       #name for barcode image
+                # switch = 0                                      #turn off switch
+                # camera.release()                                #turn off camera
+                # cv2.imwrite(image_path, frame)                  #save barcode image
+                # barcode = 0                                     #reset barcode turn
                 #flash(str(image_path))                  #shows the path
                 #print(image_path, file=sys.stderr)
                 #flask.jsonify("help") #Implement BARCODE LATERRRRRRRRRRRRRRRRRRRRRR
@@ -84,9 +116,9 @@ def index():
         else:
             pass  # unknown
     elif request.method == 'GET':
-        return render_template('capture.html')
+        return render_template('capture.html', barorexp = popups1())
 
-    return render_template("capture.html")
+    return render_template("capture.html", barorexp = popups1())
 
 @app.route('/')
 def home():
