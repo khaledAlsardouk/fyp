1db6f828c72326d63c7893587d6612ee766b2b2c
mrlemons23 <karimhamod23@gmail.com>
2022-05-16 22:03:16 +0300

    inventory linked

--- website_files/website/inventory.py
+++ website_files/website/inventory.py
@@ -11,18 +11,12 @@ Inventory1 = Blueprint("Inventory", __name__)
 
 choice = 0
 
-<<<<<<< Updated upstream
-heading = ("Item name", "Expiry", "notfication date", "Category")
+heading = ("Item name", "Expiry", "notfication date", "Category","USERID")
 data = []
-
-
 def GetALLItem():
-    items = Inventory.query.all()
+    items = Inventory.query.filter_by(user_id=current_user.id)
     for item in items:
-        data.append([str(item.id), item.Item_name, (item.Expiry).date(), (item.notfication_date).date(), item.Category])
-=======
-heading = ("Item name", "Expiry", "notfication date", "Category","User_id","usrename")
->>>>>>> Stashed changes
+        data.append([str(item.id), item.Item_name, item.Expiry, item.notfication_date, item.Category,current_user.id])
 
 
 @Inventory1.route('/inventory', methods=['GET', 'POST'])
@@ -30,23 +24,17 @@ heading = ("Item name", "Expiry", "notfication date", "Category","User_id","usre
 def inventory():
     global choice,data
     data = []
-    if choice == 0:
-<<<<<<< Updated upstream
-        GetALLItem()
-        choice = 1
+    
+    GetALLItem()
+      
     if request.method == 'POST':
         data = []
         print(request.form['clicked_btn'])
         delete = Inventory.query.filter_by(id=request.form['clicked_btn']).first()
-        Inventory.session.delete(delete)
-        Inventory.session.commit()
+        Inventory.delete(delete)
+        Inventory.commit()
+     
         GetALLItem()
         return render_template("Inventory.html", headings=heading, datas=data)
     return render_template("Inventory.html", headings=heading, datas=data)
-=======
-        items = Inventory.query.filter_by(user_id=current_user.id)
-        for item in items:         
-            data.append([item.Item_name,item.Expiry, item.notfication_date, item.Category,item.user_id,current_user.First_name])
-        choice = 1
-    return render_template("helloworld.html", headings=heading, datas=data,user=current_user)
->>>>>>> Stashed changes
+    
